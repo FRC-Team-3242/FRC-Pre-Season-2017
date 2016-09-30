c8251f14adbf6b2526ff2f71e3c8a207dbc2f9f9
Gabriel Flechas <gabrielflech@gmail.com>
2016-09-30 15:31:09 -0400

    First commit. Test.

---
+++ 1.05 Hello World/src/StudentInfoCard.java
@@ -0,0 +1,34 @@
+/**
+ * This program is intended to a variety of personal information and information 
+ * regarding this course.
+ * 
+ * Name: Gabriel Flechas
+ * Date: 9/7/2016
+ */
+public class StudentInfoCard {
+
+	public static void main(String[] args) {
+		System.out.println("***************************************************************************");
+		System.out.println("*                                                                         *");
+		System.out.println("*                            Student Information                          *");
+		System.out.println("*                                                                         *");
+		System.out.println("***************************************************************************");
+        System.out.print("Name: Gabriel Flechas    ");
+        System.out.print("Birthday: 11/23/2000    ");
+        System.out.println("Age: 16");
+        System.out.println("");
+        System.out.print("School: Forest High School    ");
+        System.out.print("Grade: 10    ");
+        System.out.println("City: Ocala");
+        System.out.println("");
+        System.out.print("Home Phone: 352-867-0276    ");
+        System.out.println("Usually Available: 4:00-6:00 PM M-F");
+        System.out.println("Cell Phone: 352-462-2181");
+        System.out.println("");
+        System.out.println("Current Math: Algebra 2");
+        System.out.println("Programming Experience: Participation in programming for my school FIRST Robotics team");
+        System.out.println("");
+        System.out.println("I am taking this course in order to learn more about programming and to get the AP credit.");
+	}
+
+}
